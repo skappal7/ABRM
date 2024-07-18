c00a60a1fd129c24967f4af4484f642142971789
Sunil Kappal <skappal7@users.noreply.github.com>
2024-07-18 20:44:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ gui = Gui(pages=pages)
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8080))
-    gui.run(host="0.0.0.0", port=port)
+    gui.run(host="0.0.0.0", port=port, use_reloader=False)
